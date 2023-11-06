cab36a3dd14aa90df745354d48ad035bc3f4b69d
Rman_stat <roman_imp@mail.ru>
2023-11-06 13:49:52 +0000

    upgrade search form

--- src/components/SearchPage.jsx
+++ src/components/SearchPage.jsx
@@ -1,7 +1,7 @@
 import style from "../styles/SearchPage.module.css";
-import searchDocument from "../media/search_document.svg";
-import searchFolders from "../media/search_folders.svg";
-import searchMan from "../media/search_man.svg";
+import document from "../media/search_document.svg";
+import folders from "../media/search_folders.svg";
+import man from "../media/search_man.svg";
 
 import SimpleSlider from "./MainPageSlider";
 import Tariff from "./MainPageTariff";
@@ -11,38 +11,30 @@ import { Link } from "react-router-dom";
 function SearchPage() {
     const store = { token: true };
     return (
-        <>
-            <div className={style.container_1}>
-                <div>
+        <div className={style.search}>
 
-                    <div className={style.header_1}>
-                        сервис по поиску публикаций<br /> о компании по его ИНН
-                    </div>
-                    <p className={style.subheader_1}>
-                        Комплексный анализ публикаций, получение данных<br />
-                        в формате PDF на электронную почту.
+            <div className={style.texts_images}>
+                <div className={style.search_text}>
+                    <h1 className={style.search_title}>
+                        Найдите необходимые<br /> данные в пару кликов.
+                    </h1>
+                    <p className={style.search_details}>
+                        Задайте параметры поиска. <br />
+                        Чем больше заполните, тем точнее поиск
                     </p>
-                    <button className={style.button_1}>
-                        {store.token ? (
-                            <Link to="/search">Запросить данные</Link>
-                        ) : (
-                            <Link to="/auth">Войти</Link>
-                        )}
-                    </button>
                 </div>
-                <img src={mainImg1} alt="" />
-            </div >
-            <div className={style.header_2}>
-                почему именно мы
+                <img src={document} alt="document" className={style.document_img} />
+                <img src={folders} alt="folders" className={style.folders_img} />
             </div>
-            <SimpleSlider />
-            <img className={style.mainImg2} src={mainImg2} alt="" />
-            <div className={style.header_2}>
-                наши тарифы
+            <div className={style.search_body}>
+                <div className={style.search_form}></div>
+                <div className={style.search_images}>
+                    <img src={man} alt="man" className={style.man_img} />
+                </div>
             </div>
-            <Tariff />
-        </>
+        </div>
     );
+
 }
 
 export default SearchPage;
